23fa388d10ad782b9d0f73370f6a55c238863ba8
RevK <github@k.gg>
2023-05-09 17:33:55 +0100

    Changed config file format - this *should* backwards compatible update the file as needed. Allows more than one account on a server using --as= to be in same file.

--- toot.c
+++ toot.c
@@ -11,6 +11,7 @@
 #include <ctype.h>
 #include <err.h>
 #include <ajlcurl.h>
+#include <sys/file.h>
 
 int debug = 0;
 
@@ -40,6 +41,7 @@ main (int argc, const char *argv[])
    const char *attach = NULL;
    const char *focus = "0,0";
    const char *poll = NULL;
+   const char *as = NULL;
    int polltime = 24 * 60 * 60;
    const char *media = NULL;
    char *status = NULL;
@@ -47,9 +49,12 @@ main (int argc, const char *argv[])
    int crop = 0;
    int expand = 0;
    int quiet = 0;
+   int authzap = 0;
+   int authdefault = 0;
    {                            // POPT
       poptContext optCon;       // context for parsing command-line options
       const struct poptOption optionsTable[] = {
+         {"as", 0, POPT_ARG_STRING, &as, 0, "As", "Tag (for multi user auth file)"},
          {"status", 0, POPT_ARG_STRING, &status, 0, "Status", "Text of status, or - for stdin, assumes a post if no --edit= set"},
          {"crop", 0, POPT_ARG_INT, &crop, 0, "Crop and add …", "Characters (e.g. 500)"},
          {"expand", 0, POPT_ARG_NONE, &expand, 0, "Expand $variable in status and allow \\n, etc."},
@@ -80,6 +85,8 @@ main (int argc, const char *argv[])
          {"redirect", 0, POPT_ARG_STRING, &redirect, 0, "Redirect URL for login", "URL"},
          {"scope", 0, POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT, &scope, 0, "Scope", "scopes"},
          {"website", 0, POPT_ARG_STRING, &website, 0, "Website (for created app)", "URL"},
+         {"zap", 0, POPT_ARG_NONE, &authzap, 0, "Use with --as=, zap from auth file"},
+         {"default", 0, POPT_ARG_NONE, &authdefault, 0, "Use with --as=, Set default in auth file"},
          {"quiet", 'q', POPT_ARG_NONE, &quiet, 0, "Quiet"},
          {"debug", 'v', POPT_ARG_NONE, &debug, 0, "Debug"},
          POPT_AUTOHELP {}
@@ -122,8 +129,59 @@ main (int argc, const char *argv[])
 
    if (access (auth_file, F_OK) && !create_app)
       errx (1, "No auth file (%s), maybe you need to --create-app", auth_file);
+
+   j_t auth = j_create ();
+   if (!access (auth_file, F_OK))
+   {
+      j_err (j_read_file (auth, auth_file));
+      j_t j = j_find (auth, "server");
+      if (j)
+      {                         // Old file format - convert to new file format
+         if (!as)
+         {
+            j_t u = j_find (auth, "username");
+            if (j_isstring (u))
+               as = j_val (u);
+         }
+         if (!as)
+            as = "*";           // default
+         j = j_store_object (auth, "_app");
+         const char *tags[] =
+            { "id", "name", "website", "redirect_uri", "client_id", "client_secret", "vapid_key", "server", "scope", NULL };
+         for (const char **tag = tags; *tag; tag++)
+         {
+            j_t t = j_find (auth, *tag);
+            if (t)
+            {
+               if (j_isstring (t))
+                  j_store_string (j, *tag, j_val (t));
+               j_delete (&t);
+            }
+         }
+         j = j_store_object (auth, as);
+         j_t n = j_first (auth);
+         while (n)
+         {
+            j_t next = j_next (n);
+            const char *tag = j_name (n);
+            if (tag && *tag != '_' && n != j)
+            {
+               if (j_isstring (n))
+                  j_store_string (j, tag, j_val (n));
+               j_delete (&n);
+            }
+            n = next;
+         }
+      }
+      const char *e = j_write_file (auth, auth_file);
+      if (e)
+         errx (1, "Failed to update %s: %s", auth_file, e);
+   }
+
    if (create_app)
    {                            // Create an app
+      if (j_get (auth, "_app"))
+         errx (1, "Application already created");
       if (!server)
          errx (1, "Specify --server");
       if (!redirect)
@@ -138,22 +196,20 @@ main (int argc, const char *argv[])
       if (debug)
          j_err (j_write_pretty (t, stderr));
       char *e = j_curl_send (curl, t, r, NULL, "https://%s/api/v1/apps", server);
+      j_delete (&t);
       if (debug)
          j_err (j_write_pretty (r, stderr));
       if (e)
          errx (1, "Failed %s", e);
       j_store_string (r, "server", server);
       j_store_string (r, "scope", scope);
-      e = j_write_file (r, auth_file);
+      j_store_json (auth, "_app", &r);  // zaps r
+      e = j_write_file (auth, auth_file);
       if (e)
          errx (1, "Failed to create %s: %s", auth_file, e);
-      j_delete (&t);
-      j_delete (&r);
       login = 1;
    }
-   j_t auth = j_create ();
-   j_err (j_read_file (auth, auth_file));
-   server = j_get (auth, "server");
+   server = j_get (auth, "_app.server");
    if (!server)
       errx (1, "Need to --create-app");
    if (login)
@@ -164,6 +220,8 @@ main (int argc, const char *argv[])
       fprintf (m, "https://%s/oauth/authorize?", server);
       void add (const char *tag, const char *val)
       {
+         if (!val)
+            return;
          fprintf (m, "%s=", tag);
          while (*val)
             if (isalnum (*val))
@@ -173,9 +231,9 @@ main (int argc, const char *argv[])
          fprintf (m, "&");
       }
       add ("response_type", "code");
-      add ("client_id", j_get (auth, "client_id"));
-      add ("redirect_uri", j_get (auth, "redirect_uri"));
-      add ("scope", j_get (auth, "scope"));
+      add ("client_id", j_get (auth, "_app.client_id"));
+      add ("redirect_uri", j_get (auth, "_app.redirect_uri"));
+      add ("scope", j_get (auth, "_app.scope"));
       add ("force_login", "true");
       add ("lang", language);
       fclose (m);
@@ -191,9 +249,9 @@ main (int argc, const char *argv[])
          r = j_create ();
       if (code)
          j_store_string (t, "code", code);
-      j_store_string (t, "client_id", j_get (auth, "client_id"));
-      j_store_string (t, "client_secret", j_get (auth, "client_secret"));
-      j_store_string (t, "redirect_uri", j_get (auth, "redirect_uri"));
+      j_store_string (t, "client_id", j_get (auth, "_app.client_id"));
+      j_store_string (t, "client_secret", j_get (auth, "_app.client_secret"));
+      j_store_string (t, "redirect_uri", j_get (auth, "_app.redirect_uri"));
       j_store_string (t, "grant_type", "authorization_code");
       j_store_string (t, "scope", j_get (auth, "scope"));
       if (debug)
@@ -206,11 +264,20 @@ main (int argc, const char *argv[])
       const char *bearer = j_get (r, "access_token");
       if (!bearer || !*bearer || strcmp (j_get (r, "token_type") ? : "", "Bearer"))
          errx (1, "Expecting bearer auth");
-      j_store_string (auth, "access_token", bearer);
-      j_store_string (auth, "created_at", j_get (r, "created_at"));
-      j_store_string (auth, "scope", j_get (r, "scope"));
-      j_store_string (auth, "username", j_get (r, "account.username"));
-      j_store_string (auth, "display_name", j_get (r, "account.display_name"));
+      if (!as)
+      {
+         j_t u = j_find (r, "account.username");
+         if (j_isstring (u))
+            as = j_val (u);
+      }
+      if (!as)
+         errx (1, "Include an --as= for this user");
+      j_t u = j_store_object (auth, as);
+      j_store_string (u, "access_token", bearer);
+      j_store_string (u, "created_at", j_get (r, "created_at"));
+      j_store_string (u, "scope", j_get (r, "scope"));
+      j_store_string (u, "username", j_get (r, "account.username"));
+      j_store_string (u, "display_name", j_get (r, "account.display_name"));
       j_delete (&t);
       j_delete (&r);
       e = j_write_file (auth, auth_file);
@@ -218,9 +285,50 @@ main (int argc, const char *argv[])
          errx (1, "Failed to create %s: %s", auth_file, e);
       return 0;
    }
-   const char *bearer = j_get (auth, "access_token");
+   if (!as)
+   {
+      j_t n = j_find (auth, "_default");
+      if (j_isstring (n))
+         as = j_val (n);
+      else
+      {
+         n = j_first (auth);
+         while (n)
+         {
+            const char *tag = j_name (n);
+            if (tag && *tag != '_' && j_isobject (n))
+            {
+               as = tag;
+               break;
+            }
+            n = j_next (n);
+         }
+         if (!as)
+            as = "*";
+      }
+   }
+   j_t authas = j_find (auth, as);
+   if (!authas)
+      errx (1, "Not found --as=%s", as);
+   if (authzap)
+   {
+      j_delete (&authas);
+      const char *e = j_write_file (auth, auth_file);
+      if (e)
+         errx (1, "Failed to create %s: %s", auth_file, e);
+      return 0;
+   }
+   if (authdefault)
+   {
+      j_store_string (auth, "_default", as);
+      const char *e = j_write_file (auth, auth_file);
+      if (e)
+         errx (1, "Failed to create %s: %s", auth_file, e);
+   }
+   const char *bearer = j_get (authas, "access_token");
    if (!bearer)
       errx (1, "Need --login or --code");
+
    {                            // Verify
       j_t r = j_create ();
       char *e = j_curl_get (curl, NULL, r, bearer, "https://%s/api/v1/apps/verify_credentials", server);
